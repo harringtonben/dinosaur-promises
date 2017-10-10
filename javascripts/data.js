7fdf1a5fbd53f9a7fbde7c0a5c61fc87c09daa36
ben harrington <harringtonben@icloud.com>
2017-10-09 20:33:54 -0500

    working through promises

--- javascripts/data.js
+++ javascripts/data.js
@@ -2,9 +2,48 @@
 
 const dom = require("./dom");
 
-var initializer = function() {
-    dom({name: "T-Rex" });
+var dinosaurs = [];
+
+//Old Crappy way of doing it
+// var dinoGetter = function() {
+//     $.ajax("./data/dinosaurs.json").done(function(data1){
+//         data1.dinosaurs1.forEach(function(dino) {
+//             dinosaurs.push(dino);
+//         });
+//         $.ajax("./data/dinosaurs.json").done(function(data2) {
+//             data2.dinosaurs2.forEach(function(dino) {
+//                 dinosaurs.push(dino);
+//             });
+//         });
+//         console.log(dinosaurs);
+//     });
+// };
+
+var firstDinosaurJSON = function() {
+    return new Promise(function(resolve, reject) {
+        $.ajax("./data/dinosaurs.json").done(function(data1) {
+            resolve(data1.dinosaurs1);  
+        }).fail(function(error1) {
+            reject(error1);
+        });
+    });
 };
 
-module.exports = {initializer};
+var dinoGetter = function() {
+    firstDinosaurJSON().then(function(results) {
+        console.log("results from dino1", results);
+    }).catch(function(error) {
+        console.log("Error from dino1", error);
+    });
+};
+
+var initializer = function() {
+    dinoGetter();
+};
+
+var getDinosaurs = function() {
+    return dinosaurs;
+};
+
+module.exports = {initializer, getDinosaurs};
 
