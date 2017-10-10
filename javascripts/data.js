3ba3cce3c9d16cdf6246c96c93bf8b6e6b4ab1e5
ben harrington <harringtonben@icloud.com>
2017-10-09 20:57:49 -0500

    more promise work

--- javascripts/data.js
+++ javascripts/data.js
@@ -29,9 +29,43 @@ var firstDinosaurJSON = function() {
     });
 };
 
+var secondDinosaurJSON = function() {
+    return new Promise(function(resolve, reject) {
+        $.ajax("./data/dinosaurs2.json").done(function(data2) {
+            resolve(data2.dinosaurs2);  
+        }).fail(function(error2) {
+            reject(error2);
+        });
+    });
+};
+
+var thirdDinosaurJSON = function() {
+    return new Promise(function(resolve, reject) {
+        $.ajax("./data/dinosaurs3.json").done(function(data3) {
+            resolve(data3.dinosaurs3);  
+        }).fail(function(error3) {
+            reject(error3);
+        });
+    });
+};
+
+// PROMISE WORKS -- STILL PYRAMID OF DOOM
 var dinoGetter = function() {
     firstDinosaurJSON().then(function(results) {
-        console.log("results from dino1", results);
+        results.forEach(function(dino) {
+            dinosaurs.push(dino);
+        });
+        secondDinosaurJSON().then(function(results2) {
+            results2.forEach(function(dino) {
+                dinosaurs.push(dino);
+            });
+        });
+            thirdDinosaurJSON().then(function(results3) {
+                results3.forEach(function(dino) {
+                    dinosaurs.push(dino);
+                });
+                console.log(dinosaurs);
+            });  
     }).catch(function(error) {
         console.log("Error from dino1", error);
     });
